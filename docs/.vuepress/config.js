bb38f71695dc20c0ac9258e54d138bcc7925ad4b
Ruben Soerdien <ruben.soerdien@hotmail.com>
2019-11-27 19:55:26 +0100

    added docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Sportscar game',
+    description: 'Development blog of my sportscar game',
+    theme: 'simple',
+    base: '/sportscar-game-blog/',
+    themeConfig: {
+      author: 'Ruben Soerdien', // will display on the page footer
+    //   navbar: { // will display below the title
+    //     YOUR_SITE_NAME: 'YOUR_SITE_URL',
+    //   }
+    }
+  }
---
+++ docs/test.md
@@ -0,0 +1,8 @@
+---
+title: Introducing Sportscar Racing Manager
+date: 2019-11-26
+---
+
+# 
+
+this is my first test post
