75499dc1f616dc66d229fa1e45a5da3f79841204
Ruben Soerdien <ruben.soerdien@hotmail.com>
2020-03-22 18:16:07 +0100

    prettier fix

--- models/entry.ts
+++ models/entry.ts
@@ -1,63 +1,63 @@
-import { Driver } from "./driver";
-import { Car } from "./car";
-import { Track } from "./track";
-import { CarState } from "./enumerations/carstate";
+import { Driver } from './driver';
+import { Car } from './car';
+import { Track } from './track';
+import { CarState } from './enumerations/carstate';
 
 export class Entry {
-    category: string;
-    drivers: Driver[];
-    entryNumber: (string | number);
-    currentDriverIndex: number;
-    car: Car;
-    track: Track;
-    state: CarState;
+  category: string;
+  drivers: Driver[];
+  entryNumber: string | number;
+  currentDriverIndex: number;
+  car: Car;
+  track: Track;
+  state: CarState;
 
-    constructor(entryNumber: (string | number), car: Car) {
-        this.drivers = [];
-        this.currentDriverIndex = 0;
-        this.entryNumber = entryNumber;
-        this.car = car;
-        this.car.entry = this;
-        this.state = CarState.GARAGE;
+  constructor(entryNumber: string | number, car: Car) {
+    this.drivers = [];
+    this.currentDriverIndex = 0;
+    this.entryNumber = entryNumber;
+    this.car = car;
+    this.car.entry = this;
+    this.state = CarState.GARAGE;
+  }
+
+  handle() {
+    switch (this.state) {
+      case CarState.GARAGE:
+        break;
+      case CarState.OFF_TRACK:
+        break;
+      case CarState.ON_TRACK:
+        break;
+      case CarState.PITBOX:
+        break;
     }
 
-    handle() {
-        switch (this.state) {
-            case CarState.GARAGE:
-                break;
-            case CarState.OFF_TRACK:
-                break;
-            case CarState.ON_TRACK:
-                break;
-            case CarState.PITBOX:
-                break;
-        }
+    this.GetActiveDriver().handle(this.car);
+  }
 
-        this.GetActiveDriver().handle(this.car);
-    }
+  RunTelemetry() {
+    // in the future this will be a list of ITelemetry items that will be looped through
+    // this.telemetry.handle();
+    this.car.laps[this.car.GetLaps()].handle();
+  }
 
-    RunTelemetry() {
-        // in the future this will be a list of ITelemetry items that will be looped through
-        // this.telemetry.handle();
-        this.car.laps[this.car.GetLaps()].handle();
-    }
+  GetActiveDriver() {
+    return this.drivers[this.currentDriverIndex];
+  }
 
-    GetActiveDriver() {
-        return this.drivers[this.currentDriverIndex];
-    }
-
-    // send data
-    ToJson() {
-        return {
-            EntryNumber: this.entryNumber,
-            category: this.category,
-            drivers: this.drivers,
-            currentDriverIndex: this.currentDriverIndex,
-            car: {
-                chassis: this.car.chassis.name,
-                category: this.car.entry.category,
-                extra: this.car.ToJSON()
-            }
-        };
-    }
+  // send data
+  ToJson() {
+    return {
+      EntryNumber: this.entryNumber,
+      category: this.category,
+      drivers: this.drivers,
+      currentDriverIndex: this.currentDriverIndex,
+      car: {
+        chassis: this.car.chassis.name,
+        category: this.car.entry.category,
+        extra: this.car.ToJSON(),
+      },
+    };
+  }
 }
--- models/pitlane.ts
+++ models/pitlane.ts
@@ -1,8 +1,9 @@
-import { IPitlane } from "./interfaces/pitlane";
+import { IPitlane } from './interfaces/pitlane';
 
-export class PitLane implements IPitlane{
-    entryDistance: number;    exitDistance: number;
-    length: number;
-    maxSpeed: number;
-    boxLocations: [number];
+export class PitLane implements IPitlane {
+  entryDistance: number;
+  exitDistance: number;
+  length: number;
+  maxSpeed: number;
+  boxLocations: [number];
 }
